674ea880846b1fa26e176e31beff75bf791fb24d
Tony Skidmore <16082810+tonyskidmore@users.noreply.github.com>
2023-03-12 20:42:37 +0000

    adding output condition

--- scripts/commit_files_changed.sh
+++ scripts/commit_files_changed.sh
@@ -5,4 +5,4 @@ commit_updated_files=$(git diff-tree --no-commit-id --name-only "$GITHUB_SHA" -r
 
 echo "$commit_updated_files"
 
-echo "changed_files=$commit_updated_files" >> "$GITHUB_OUTPUT"
+[[ -n "$commit_updated_files" ]] && echo "changed_files=$commit_updated_files" >> "$GITHUB_OUTPUT"
