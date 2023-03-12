5ed19eb28ee4ab5d256aa7ef02f3049046632011
Tony Skidmore <16082810+tonyskidmore@users.noreply.github.com>
2023-03-12 20:36:22 +0000

    adding echo

--- scripts/commit_files_changed.sh
+++ scripts/commit_files_changed.sh
@@ -2,4 +2,7 @@
 set -e
 
 commit_updated_files=$(git diff-tree --no-commit-id --name-only "$GITHUB_SHA" -r)
+
+echo "$commit_updated_files"
+
 echo "changed_files=$commit_updated_files" >> "$GITHUB_OUTPUT"
