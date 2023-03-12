3148a4f0c6d4ed957c64d0777507c7287f24af66
Tony Skidmore <16082810+tonyskidmore@users.noreply.github.com>
2023-03-12 19:02:04 +0000

    updating directories

--- environments/dev/azure/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name   = "rg-terraform-state"
-    storage_account_name  = "satfbackendstate123dev"
-    container_name        = "tfstate"
-    key                   = "terraform.tfstate"
-  }
-}
