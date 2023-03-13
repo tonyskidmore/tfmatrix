520c227042159070d5289148905b86bb6446fb45
Tony Skidmore <16082810+tonyskidmore@users.noreply.github.com>
2023-03-13 12:54:02 +0000

    updating config handling

--- scripts/get_config.sh
+++ scripts/get_config.sh
@@ -2,10 +2,23 @@
 
 GITHUB_WORKSPACE=${GITHUB_WORKSPACE:-.}
 
-matrix_config=$(yq 'del(."changed_only")' -oj "$GITHUB_WORKSPACE/config.yml")
+config=$(< "$GITHUB_WORKSPACE/config.yml")
+
+changed_only=$(yq '.changed_only' <<< "$config")
+targets=($(yq -r '.targets[]' <<< "$config"))
+environments=($(yq -r '.environments[]' <<< "$config"))
+
+matrix_config=$(yq 'del(."changed_only")' -oj <<< "$config")
 config_compact_json=$(jq -c <<< "$matrix_config")
 
-changed_only=$(yq '.changed_only' config.yml)
+# changed_only=$(yq '.changed_only' config.yml)
+
+# matrix_config=$(yq 'del(."changed_only")' -oj "$GITHUB_WORKSPACE/config.yml")
+# config_compact_json=$(jq -c <<< "$matrix_config")
+# targets=$(yq -r '.targets[]' config.yml)
+
+declare -p targets
+declare -p environments
 
 printf "matrix config: %s\n" "matrix_config"
 printf "compact json: %s\n" "$config_compact_json"
@@ -15,4 +28,3 @@ printf "CHANGED_FILES: \n%s\n" "$CHANGED_FILES"
 #TODO: handle changed files processing
 
 echo "config=$config_compact_json" >> "$GITHUB_OUTPUT"
-
