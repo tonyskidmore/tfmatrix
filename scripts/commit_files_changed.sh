e9bf8166e33632ab82068a0f227711644ecf4ca7
Tony Skidmore <16082810+tonyskidmore@users.noreply.github.com>
2023-03-13 10:21:01 +0000

    updating output

--- scripts/commit_files_changed.sh
+++ scripts/commit_files_changed.sh
@@ -5,4 +5,9 @@ commit_updated_files=$(git diff-tree --no-commit-id --name-only "$GITHUB_SHA" -r
 
 echo "$commit_updated_files"
 
-[[ -n "$commit_updated_files" ]] && echo "changed_files=$commit_updated_files" >> "$GITHUB_OUTPUT"
+if [[ -n "$commit_updated_files" ]]
+then
+  echo "changed_files=$commit_updated_files" >> "$GITHUB_OUTPUT"
+else
+  echo "changed_files='none'" >> "$GITHUB_OUTPUT"
+fi
